365d244d13473dddc71e0ede402467663f5108ce
nigerabed <nigerabed@gmail.com>
2025-02-14 13:55:08 +0100

    hide tilmeldt button for instrcutor

--- src/app/activitier/[id]/page.js
+++ src/app/activitier/[id]/page.js
@@ -10,44 +10,42 @@ import { serverFetch, serverFetchWithAuth } from "@/lib/server-fetch";
 import { cookies } from "next/headers";
 import Image from "next/image";
 
-
 export default async function ActivityDetails({ params }) {
-
   const baseUrl = process.env.NEXT_PUBLIC_LANDRUP_API_BASE_URL;
- 
 
   console.log("baseUrl", baseUrl);
 
   const cookieStore = await cookies();
 
-  const activityId = params?.id;
+  const activityId = await params?.id;
 
-  const data = await serverFetch(
-    `${baseUrl}/api/v1/activities/${activityId}`
-  );
+  const data = await serverFetch(`${baseUrl}/api/v1/activities/${activityId}`);
+  console.log("dataaaaaaaaaaaa", data);
 
-  const url =data.asset.url;
+  const url = data.asset.url;
   const newUrl = baseUrl + url.slice("http://localhost:4000".length);
 
   const userId = cookieStore.get("landrup_userid");
   const token = cookieStore.get("landrup_token");
+  const role = cookieStore.get("landrup_role");
   let isTilmeldt = false;
 
-  if(userId && token){
-
-    // serverFetchWithAuth this function fetch data with api and token ( jeg har brught den function i 
+  if (userId && token) {
+    // serverFetchWithAuth this function fetch data with api and token ( jeg har brught den function i
     // tilmeld activityDetails fordi skal jeg brug user api med token)
 
     const userData = await serverFetchWithAuth(
       `${baseUrl}/api/v1/users/${userId.value}`,
       token.value
     );
-  
-    const tilmeldtActivity = userData.activities.filter((act) => activityId == act.id);
-    
+
+    const tilmeldtActivity = userData.activities.filter(
+      (act) => activityId == act.id
+    );
+
     if (tilmeldtActivity.length > 0) {
       isTilmeldt = true;
-    } 
+    }
   }
 
   return (
@@ -61,7 +59,15 @@ export default async function ActivityDetails({ params }) {
             height={150}
             className="h-[30em] w-full object-cover"
           />
-          {!isTilmeldt ? <TidmeldButton activityId={activityId} /> : <div className="absolute bottom-7 left-[6em]"> <Button text ={"Forlad"} /> </div>}
+
+          {role.value === "instructor"?(<></>) : !isTilmeldt ? (
+            <TidmeldButton activityId={activityId} />
+          ) : (
+            <div className="absolute bottom-7 left-[6em]">
+              <Button text={"Forlad"} />
+            </div>
+          )}
+         
         </div>
         <div className="p-[2em]">
           <h2 className="text-white text-[1.9em] font-semibold">{data.name}</h2>
