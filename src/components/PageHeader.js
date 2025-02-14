0ae251ff1ab308d65f79960a3d13f61b6b2418cd
nigerabed <nigerabed@gmail.com>
2025-02-14 13:57:37 +0100

    better font size for page header

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,7 +2,7 @@ export default function PageHeader({indhold}) {
     return (
       <header >
   
-        <div className=" text-white font-semibold text-[2.5em] pl-[1em] pt-[1em]">
+        <div className=" text-white font-medium text-[2.5em] pl-[1em] pt-[1em]">
           {indhold}
         </div>
       </header>
